f1f1142c559c73d9819970d8a586ff6fd70f55c1
Mickael Pham <mickael.pham@gmail.com>
2019-04-17 12:22:18 -0700

    Allow ProductRatePlanChargeTiers to be live-queried (#25)
    * v3.0.2
    
    * Allow ProductRatePlanChargeTiers to be live-queried
    
    * Change wording
    
    * Update lib/iron_bank/resources/product_rate_plan_charge_tier.rb
    
    Co-Authored-By: mickaelpham <mickael.pham@gmail.com>

--- lib/iron_bank/resources/product_rate_plan_charge_tier.rb
+++ lib/iron_bank/resources/product_rate_plan_charge_tier.rb
@@ -6,6 +6,20 @@ module IronBank
     # charge.
     #
     class ProductRatePlanChargeTier < Resource
+      # These fields are declared as `<selectable>true</selectable>` but only
+      # for the `export` context (ie., via ZOQL export). To let live queries go
+      # through when the local records are not exported, we only allow querying
+      # the non-discount tiers.
+      def self.exclude_fields
+        %w[
+          Active
+          IncludedUnits
+          OveragePrice
+          DiscountAmount
+          DiscountPercentage
+        ]
+      end
+
       with_schema
       with_local_records
       with_cache
