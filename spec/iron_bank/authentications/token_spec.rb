1fab347890a6d048febdc8f6a2657e03655ad337
Mickaël Pham <mickael@zendesk.com>
2019-09-18 11:20:34 -0700

    Refactor faraday configuration spec into a shared example
    (cherry picked from commit 3b0a2dabe97a10300c34479c6277bd1af1ef6774)

--- spec/iron_bank/authentications/token_spec.rb
+++ spec/iron_bank/authentications/token_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "timecop"
+require "shared_examples/faraday_connection"
 
 RSpec.describe IronBank::Authentications::Token do
   let(:initial_token) do
@@ -61,15 +62,10 @@ RSpec.describe IronBank::Authentications::Token do
   subject { described_class.call(credentials) }
 
   before do
-    allow(Faraday).to receive(:new).and_yield(connection).and_return(connection)
+    allow(Faraday).to receive(:new).and_return(connection)
 
-    # Faraday::Connection configuration
-    allow(connection).to receive(:request)
-    allow(connection).to receive(:response)
-    allow(connection).to receive(:adapter)
-
-    # Faraday::Connection POST request
-    allow(connection).to receive(:post).
+    allow(connection).
+      to receive(:post).
       and_return(first_response, second_response)
   end
 
@@ -151,56 +147,7 @@ RSpec.describe IronBank::Authentications::Token do
         expect(expired.expired?).to be_truthy
       end
     end
-
-    # FIXME: Extract in a shared example since it's also used in cookie_spec.rb
-    describe "#connection" do
-      subject(:faraday_connection) do
-        described_class.new(credentials).send(:connection)
-      end
-
-      it "uses URL encoded format for the request" do
-        faraday_connection
-        expect(connection).to have_received(:request).with(:url_encoded)
-      end
-
-      it "set the response to be parsed using JSON" do
-        faraday_connection
-        expect(connection).to have_received(:request).with(:url_encoded)
-      end
-
-      it "uses the default Faraday adapter" do
-        faraday_connection
-
-        expect(connection).
-          to have_received(:adapter).
-          with(Faraday.default_adapter)
-      end
-
-      describe "IronBank configurable middlewares" do
-        before do
-          allow(connection).to receive(:use)
-        end
-
-        context "no middleware configured" do
-          it "does not configure the Faraday::Connection to use any" do
-            faraday_connection
-
-            expect(connection).to_not have_received(:use)
-          end
-        end
-
-        context "with a middleware configured" do
-          before { IronBank.configuration.middlewares = %i[Foo Bar] }
-          after  { IronBank.configuration.middlewares = [] }
-
-          it "configures the Faraday::Connection to use the middlewares" do
-            faraday_connection
-
-            expect(connection).to have_received(:use).with(:Foo, nil)
-            expect(connection).to have_received(:use).with(:Bar, nil)
-          end
-        end
-      end
-    end
   end
+
+  include_examples "Faraday::Connection configuration block"
 end
