3c08a0069a8063115ad8ac04f1d0a0850b41ddc6
Mickaël Pham <mickael@zendesk.com>
2019-09-19 09:29:22 -0700

    Add communication profile resource

---
+++ lib/iron_bank/resources/communication_profile.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module IronBank
+  module Resources
+    # A Zuora communication profile. Controls which notifications (emails and
+    # callouts) are sent to a customers and external systems, e.g. provisioning,
+    # when events happen for the account, such as invoice posted, subscription
+    # created, etc.
+    #
+    class CommunicationProfile < Resource
+      with_schema
+
+      with_many :accounts
+    end
+  end
+end
