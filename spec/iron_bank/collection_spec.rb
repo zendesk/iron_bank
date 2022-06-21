de48ba28b5caf985bdfff168dea2c3cfd4580819
Zach Dennis <zach.dennis@gmail.com>
2022-06-21 17:43:11 -0400

    IronBank::Collection has learned #to_ary in order to support implicit array conversions
    which allows things like the below to work:
    
       # Pull out the first three records from the IronBank::Collection instance.
       a, b, c = some_iron_bank_collection
    
       # Any method that relied on implicit conversion now works as expected.
       # The below will now return a flattened array of IronBank::Resource::RatePlan.
       account = IronBank::Account.find(...)
       rate_plans = account.subscriptions.flat_map(&:rate_plans)
    
    Prior to this change anything that relied on implicit conversion returned an IronBank::Collection which led to some strange results like:
    
       # This returned an array of IronBank::Collection instances.
       account = IronBank::Account.find(...)
       rate_plans = account.subscriptions.flat_map(&:rate_plans)

--- spec/iron_bank/collection_spec.rb
+++ spec/iron_bank/collection_spec.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.describe IronBank::Collection do
-  let(:klass) { IronBank::Resource }
-  let(:records) { [] }
+  let(:klass) do
+    Class.new(IronBank::Resource) do
+      def related_records
+        remote[:related_records]
+      end
+    end
+  end
+  let(:records) { build_resources }
   let(:conditions) { { account_id: "123" } }
   let(:subject) { described_class.new(klass, conditions, records) }
 
+  def build_resources(*resource_attributes)
+    resource_attributes.map { |attributes| klass.new(attributes) }
+  end
+
   describe "#reload" do
-    let(:new_records) { [{ id: "xyz", name: "Foo Loo" }] }
+    let(:new_records) { build_resources({ id: "xyz", name: "Foo Loo" }) }
     let(:reload) { subject.reload }
 
     it "makes a live where query" do
@@ -26,6 +36,52 @@ RSpec.describe IronBank::Collection do
 
   describe "#to_a" do
     it { expect(subject.to_a).to eq(records) }
+  end
+
+  describe "implicit conversions" do
+    let(:records) do
+      build_resources(
+        { id: "1", related_records: described_class.new(klass, conditions, build_resources(*fruits)) },
+        { id: "2", related_records: described_class.new(klass, conditions, build_resources(*vegetables)) },
+        { id: "3", related_records: described_class.new(klass, conditions, build_resources(*nonedibles)) }
+      )
+    end
+
+    let(:fruits) { [{ id: "1", name: "Apples" }, { id: "2", name: "Bananas" }] }
+    let(:vegetables) { [{ id: "3", name: "Carrots" }, { id: "7", name: "Onions" }] }
+    let(:nonedibles) { [{ id: "5", name: "Rocks" }, { id: "6", name: "Bark" }] }
+
+    it "supports implicit conversion to an array" do
+      first, second, third = subject
+
+      expect(first).to eq records[0]
+      expect(second).to eq records[1]
+      expect(third).to eq records[2]
+    end
+
+    it "works with methods that rely on implicit array conversion under the hood such as #flat_map" do
+      expected_records = build_resources(*[fruits, vegetables, nonedibles].flatten)
+      expect(records.flat_map(&:related_records)).to eq expected_records
+    end
+  end
+
+  describe "#flat_map" do
+    let(:records) do
+      build_resources(
+        { id: "1", related_records: described_class.new(klass, conditions, build_resources(*fruits)) },
+        { id: "2", related_records: described_class.new(klass, conditions, build_resources(*vegetables)) },
+        { id: "3", related_records: described_class.new(klass, conditions, build_resources(*nonedibles)) }
+      )
+    end
+
+    let(:fruits) { [{ id: "1", name: "Apples" }, { id: "2", name: "Bananas" }] }
+    let(:vegetables) { [{ id: "3", name: "Carrots" }, { id: "7", name: "Onions" }] }
+    let(:nonedibles) { [{ id: "5", name: "Rocks" }, { id: "6", name: "Bark" }] }
+
+    it "works correctly" do
+      expected_records = build_resources(*[fruits, vegetables, nonedibles].flatten)
+      expect(records.flat_map(&:related_records)).to eq expected_records
+    end
   end
 
   describe "delegates object methods to records" do
