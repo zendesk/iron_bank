4e87410533347eb49fc3f97f50e41cea6583efed
Lea Roy Klinginsmith <learoy.klinginsmith@zendesk.com>
2024-10-16 15:44:41 -0600

    Update faraday to 2.x

--- lib/iron_bank/faraday_middleware/response/raise_error.rb
+++ lib/iron_bank/faraday_middleware/response/raise_error.rb
@@ -8,7 +8,7 @@ module IronBank
     module Response
       # This class raises an exception based on the HTTP status code and the
       # `success` flag (if present in the response) from Zuora.
-      class RaiseError < Faraday::Response::Middleware
+      class RaiseError < Faraday::Middleware
         def on_complete(env)
           (error = IronBank::Error.from_response(env.response)) && raise(error)
         end
